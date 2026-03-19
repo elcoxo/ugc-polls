a4d99a1289aa64c38e85b1434c3e5e9dadacaf69
elcoxo <rakkii7777@gmail.com>
2026-03-20 01:46:19 +0300

    Add poll session admin inline

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,7 @@ class QuestionInline(admin.TabularInline):
     model = Question
     fields = ['text', 'ordering']
     ordering = ['ordering']
+    extra = 0
     show_change_link = True
 
 
@@ -15,6 +16,7 @@ class AnswerOptionInline(admin.TabularInline):
     model = AnswerOption
     fields = ['text', 'order']
     ordering = ['order']
+    extra = 0
 
 
 @admin.register(Poll)
@@ -82,11 +84,28 @@ class UserResponseAdmin(admin.ModelAdmin):
         return super().get_queryset(request).select_related('session', 'question', 'option')
 
 
+class UserResponseInline(admin.TabularInline):
+    model = UserResponse
+    fields = ['question_link', 'option_link', 'created_at']
+    readonly_fields = ['question_link', 'option_link', 'created_at']
+    extra = 0
+    can_delete = False
+
+    def question_link(self, obj):
+        url = f'/admin/polls/question/{obj.question_id}/change/'
+        return format_html('<a href="{}">{}</a>', url, obj.question)
+
+    def option_link(self, obj):
+        url = f'/admin/polls/answeroption/{obj.option_id}/change/'
+        return format_html('<a href="{}">{}</a>', url, obj.option)
+
+
 @admin.register(PollSession)
 class PollSessionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'poll', 'created_at', 'finished_at']
+    inlines = [UserResponseInline]
+    list_display = ['id', 'slug', 'user', 'poll', 'created_at', 'finished_at']
     list_filter = ['created_at', 'finished_at']
-    readonly_fields = ['id', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'slug', 'created_at', 'updated_at']
     raw_id_fields = ['user', 'poll']
 
     def get_queryset(self, request):
