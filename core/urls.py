75e662b137dee185a957ae9468aad17461ee1334
elcoxo <rakkii7777@gmail.com>
2026-03-20 04:03:40 +0300

    Add simple register form

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from polls.views import PollViewSet, PollSessionViewSet
+from users.views import register
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename='polls')
@@ -19,6 +20,7 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('register/', register, name='register'),
     path('', TemplateView.as_view(template_name='polls/list.html'), name='poll-list'),
     path('polls/<slug:slug>/', TemplateView.as_view(template_name='polls/detail.html'), name='poll-detail'),
 ]
